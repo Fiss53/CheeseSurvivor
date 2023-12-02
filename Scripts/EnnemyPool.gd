dd1e80278fd59f5ba9adc1e336715a0ed741655a
Psycho_Pat <patrick.charon@murena.io>
2023-12-02 21:50:26 +0100

    Modification de la répartition du spawn

--- Scripts/EnnemyPool.gd
+++ Scripts/EnnemyPool.gd
@@ -5,10 +5,14 @@ extends Node2D
 func _process(_delta):
 	var ennemies_count = get_child_count()
 	if (ennemies_count < MAX_ENNEMIES):
-		var angle = randf_range(0, 2 * PI)
+		var r_min = 900
+		var r_max = 1900
 		
-		var ennemy_x = int(cos(angle) * 900)
-		var ennemy_y = int(sin(angle) * 900)
+		var angle = randf_range(0, 2 * PI)
+		var r = randf_range(r_min, r_max)
+		
+		var ennemy_x = int(cos(angle) * r)
+		var ennemy_y = int(sin(angle) * r)
 		
 		var ennemy = load("res://Scenes/Ennemy.tscn").instantiate()
 		ennemy.position.x = $"../Player".position.x + ennemy_x
