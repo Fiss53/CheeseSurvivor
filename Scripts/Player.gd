ab4f2061908557cdb9615444031f403a85940a13
Yvan <yvandouis@hotmail.fr>
2023-12-02 18:50:10 +0100

    petit ajout

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 @onready var animated_sprite = get_node("AnimatedSpritePlayer")
 @export var SPEED = 2
+var direction
 func _process(delta):
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
-	var direction = Vector2(direction_x,direction_y)
+	direction = Vector2(direction_x,direction_y)
 	match(direction):
 		Vector2.ZERO:
 			animated_sprite.stop()
