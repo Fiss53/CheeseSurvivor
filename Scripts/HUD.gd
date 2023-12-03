8d6a4c8168d1817669001707b8bcf7758d30f476
Yvan <yvandouis@hotmail.fr>
2023-12-03 13:24:42 +0100

    HUD amélioration du code + PHI.png

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -2,7 +2,10 @@ extends CanvasLayer
 
 # Notifies `Main` node that the button has been pressed
 signal start_game
-
+@onready var message = $Message
+@onready var start_button = $StartButton
+@onready var score_Label = $ScoreLabel
+@onready var MessageTimer = $MessageTimer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,27 +15,27 @@ func _process(delta):
 	pass
 
 func show_message(text):
-	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
+	message.text = text
+	message.show()
+	MessageTimer.start()
 	
 func show_game_over():
 	show_message("Game Over")
 	# Wait until the MessageTimer has counted down.
-	await $MessageTimer.timeout
+	await MessageTimer.timeout
 
-	$Message.text = "Dodge the Creeps!"
-	$Message.show()
+	message.text = "Dodge the Creeps!"
+	message.show()
 	# Make a one-shot timer and wait for it to finish.
 	await get_tree().create_timer(1.0).timeout
-	$StartButton.show()
+	start_button.start_button()
 	
 func update_score(score):
-	$ScoreLabel.text = str(score)
+	score_Label.text = str(score)
 
 func _on_start_button_pressed():
-	$StartButton.hide()
+	start_button.hide()
 	start_game.emit()
 
 func _on_message_timer_timeout():
-	$Message.hide()
+	message.hide()
